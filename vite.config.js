c965630f39395f1682b11fa8cceeff7ac0f89b69
Parv <parvruhil68@gmail.com>
2024-09-23 21:59:31 +0530

    vite file

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/react-password-genertor/',
+  base: '/react-password-generator/',  // Corrected base path
 });
+
 
